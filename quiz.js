de1905eb53a0ccc99d0381b1136525dfdfaf0957
drissnafii <drissnafi3@gmail.com>
2024-12-30 16:41:00 +0100

    commit ..

---
+++ quiz.js
@@ -0,0 +1,25 @@
+const questions = [
+    "En JavaScript, 'null' est un type d'objet.",
+    "Les tableaux en JavaScript commencent à l'index 1.", 
+    "La méthode 'map()' peut être utilisée pour transformer un tableau.", 
+    "En JavaScript, '==' compare les valeurs avec conversion de type automatique.", 
+    "La méthode 'querySelector()' permet de sélectionner plusieurs éléments à la fois.", 
+    "Le mot-clé 'const' permet de déclarer une variable modifiable.", 
+    "La fonction 'setTimeout()' est utilisée pour exécuter du code après un délai donné.", 
+    "Les promesses en JavaScript utilisent 'then()' et 'catch()' pour la gestion des résultats.", 
+    "Une fonction fléchée (arrow function) a sa propre valeur 'this'.", 
+];
+
+let myVrai = document.getElementById('true-btn');
+
+myVrai.addEventListener("click", () => {
+    document.getElementById("question").textContent = questions[0];
+    switch (questions) {
+        case value:
+            break;
+        default:
+            break;
+    }
+});
+
+
